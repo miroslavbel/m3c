15ad97d3a9dd30b5842a8d9a510fc00a2cf06aff
Belov Miroslav <miroslavbelov8@gmail.com>
2023-08-08 21:40:48 +0400

    [M3C-31] impl UTF8GetASCIICodepointWithLen

---
+++ include/m3c/common/unicode.h
@@ -0,0 +1,16 @@
+#ifndef _M3C_INCGUARD_UNICODE_H
+#define _M3C_INCGUARD_UNICODE_H
+
+#include <m3c/common/types.h>
+
+/**
+ * \brief Unicode code point.
+ */
+typedef m3c_u32 M3C_UCP;
+
+/**
+ * \brief Replacement character code point.
+ */
+#define M3C_REPLACEMENT_CHARACTER_UCP 0xFFFD
+
+#endif /* _M3C_INCGUARD_UNICODE_H */
