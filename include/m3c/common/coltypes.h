5861d9b98310a9396c3ff187be3596fbec01ad0d
Belov Miroslav <miroslavbelov8@gmail.com>
2023-11-10 16:11:58 +0400

    [M3C-56] add VEC_RESERVE

--- include/m3c/common/coltypes.h
+++ include/m3c/common/coltypes.h
@@ -37,6 +37,20 @@ M3C_VEC_Push_impl(
 );
 
 /**
+ * \brief Increase the capacity of the vector to a value that's greater or equal to `newCap`.
+ *
+ * \param[in,out] buf      pointer to the pointer to the buffer
+ * \param[in,out] cap      pointer to the buffer capacity
+ * \param         elemSize size of the vector element in bytes
+ * \param         newCap   new capacity of the buffer, in number of elements
+ * \return
+ * + M3C_ERROR_OK
+ * + M3C_ERROR_OOM - if failed to realloc
+ */
+M3C_ERROR
+M3C_VEC_Reserve_impl(void **buf, m3c_size_t *cap, m3c_size_t elemSize, m3c_size_t newCap);
+
+/**
  * \brief Pushes `ELEM` to the `VEC`.
  *
  * \details If it is necessary to reallocate the buffer, sets the new capacity to twice the previous
@@ -53,5 +67,18 @@ M3C_VEC_Push_impl(
  */
 #define M3C_VEC_PUSH(TYPE, VEC, ELEM)                                                              \
     M3C_VEC_Push_impl((void **)&(VEC)->data, &(VEC)->len, &(VEC)->cap, ELEM, sizeof(TYPE))
+
+/**
+ * \brief Increase the capacity of `VEC` to a value that's greater or equal to `NEW_CAP`.
+ *
+ * \param         TYPE    type of vector element
+ * \param[in,out] VEC     pointer to the vector struct
+ * \param         NEW_CAP new capacity of the vector, in number of elements
+ * \return
+ * + M3C_ERROR_OK
+ * + M3C_ERROR_OOM - if failed to realloc
+ */
+#define M3C_VEC_RESERVE(TYPE, VEC, NEW_CAP)                                                        \
+    M3C_VEC_Reserve_impl((void **)&(VEC)->data, &(VEC)->cap, sizeof(TYPE), (NEW_CAP))
 
 #endif /* _M3C_INCGUARD_COLTYPES_H */
--- src/common/coltypes.c
+++ src/common/coltypes.c
@@ -8,20 +8,31 @@ M3C_VEC_Push_impl(
     void **buf, m3c_size_t *len, m3c_size_t *cap, void const *elem, m3c_size_t elemSize
 ) {
     m3c_size_t newCap;
-    void *newPtr;
 
     if (*len == *cap) {
-        newCap = *cap + *cap;
+        newCap = *cap == 0 ? 1 : *cap + *cap;
 
-        newPtr = m3c_realloc(*buf, elemSize * newCap);
-        if (!newPtr)
+        if (M3C_VEC_Reserve_impl(buf, cap, elemSize, newCap) == M3C_ERROR_OOM)
             return M3C_ERROR_OOM;
-
-        *cap = newCap;
-        *buf = newPtr;
     }
 
     m3c_memcpy((char *)*buf + elemSize * (*len), elem, elemSize);
     ++(*len);
+    return M3C_ERROR_OK;
+}
+
+M3C_ERROR
+M3C_VEC_Reserve_impl(void **buf, m3c_size_t *cap, m3c_size_t elemSize, m3c_size_t newCap) {
+    void *newPtr;
+
+    if (*cap >= newCap)
+        return M3C_ERROR_OK;
+
+    newPtr = m3c_realloc(*buf, elemSize * newCap);
+    if (!newPtr)
+        return M3C_ERROR_OOM;
+    *buf = newPtr;
+    *cap = newCap;
+
     return M3C_ERROR_OK;
 }
