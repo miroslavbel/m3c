1c3b9816e9b247190132593cf2d33387e0446e4a
Belov Miroslav <miroslavbelov8@gmail.com>
2023-12-06 23:54:55 +0400

    [M3C-65][common] impl BSearch_by_key

--- src/common/coltypes.c
+++ src/common/coltypes.c
@@ -4,6 +4,8 @@
 #include <m3c/rt/alloc.h>
 #include <m3c/rt/mem.h>
 
+void const *M3C_EchoFn(void const *obj, void const *arg) { return obj; }
+
 M3C_ERROR
 M3C_VEC_Push_impl(
     void **buf, m3c_size_t *len, m3c_size_t *cap, void const *elem, m3c_size_t elemSize
@@ -39,9 +41,10 @@ M3C_VEC_Reserve_impl(void **buf, m3c_size_t *cap, m3c_size_t elemSize, m3c_size_
 }
 
 M3C_ERROR M3C_ARR_BSearch_impl(
-    void const *buf, m3c_size_t len, m3c_size_t elemSize, void const *elem,
-    int (*cmp)(void const *, void const *), m3c_size_t *n
+    void const *buf, m3c_size_t len, m3c_size_t elemSize, void const *elem, M3C_CMP_FN *cmpFn,
+    m3c_size_t *n, M3C_KEY_FN keyFn, void *keyArg
 ) {
+    void const *arrElem;
     int cmpRes;
     m3c_size_t max;
     m3c_size_t min = 0;
@@ -52,18 +55,26 @@ M3C_ERROR M3C_ARR_BSearch_impl(
     }
     max = len - 1;
 
+    if (!keyFn)
+        keyFn = (const void *(*)(const void *, void *))M3C_EchoFn;
+
     M3C_LOOP {
         *n = (max - min) / 2 + min;
 
-        cmpRes = cmp((m3c_u8 const *)buf + elemSize * *n, elem);
+        arrElem = (m3c_u8 const *)buf + elemSize * *n;
+
+        cmpRes = cmpFn(keyFn(arrElem, keyArg), elem);
         if (cmpRes == 0)
             return M3C_ERROR_OK;
+
         else if (max == min) {
             if (max + 1 == len && cmpRes < 0)
                 ++(*n);
             return M3C_ERROR_NOT_FOUND;
+
         } else if (cmpRes > 0)
             max = *n;
+
         else
             min = *n + 1;
     }
