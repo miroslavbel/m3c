c5c8ab845080e6479effcb6bfe3bad3e955b385a
Belov Miroslav <miroslavbelov8@gmail.com>
2024-01-23 21:03:11 +0400

    [M3C-90][common] GNUC and CLANG

--- include/m3c/common/babel.h
+++ include/m3c/common/babel.h
@@ -1,13 +1,15 @@
 #ifndef _M3C_INCGUARD_BABEL_H
 #define _M3C_INCGUARD_BABEL_H
 
+#include <m3c/common/env.h>
+
 #if __STDC_VERSION__ >= 199901L
 #    define m3c_restrict restrict
 #else
 #    define m3c_restrict /* just ignore before C99 */
 #endif
 
-#if defined(__GNUC__) || defined(__clang__)
+#if defined(M3C_GNUC) || defined(M3C_CLANG)
 #    define M3C_SYSV_ABI __attribute__((sysv_abi))
 #endif
 
