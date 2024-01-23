40b66e43edd68789f915e103287f306afa673929
Belov Miroslav <miroslavbelov8@gmail.com>
2024-01-23 21:06:18 +0400

    [M3C-90][rt] add M3C_FEATURE_USE_COMPILER_BUILTIN_FUNCTIONS feature

--- include/m3c/rt/mem.h
+++ include/m3c/rt/mem.h
@@ -1,14 +1,25 @@
 #ifndef _M3C_INCGUARD_RT_MEM_H
 #define _M3C_INCGUARD_RT_MEM_H
 
+#include <m3c/common/env.h>
 #include <m3c/common/types.h>
 #include <m3c/common/babel.h>
 
-#include <string.h>
+#ifdef M3C_FEATURE_USE_COMPILER_BUILTIN_FUNCTIONS
 
-#define m3c_memcpy(dest, src, count) memcpy(dest, src, count)
+#    if defined(M3C_GNUC) || defined(M3C_CLANG)
 
-#define m3c_memmove(dest, src, count) memmove(dest, src, count)
+/* See:
+ * + https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+ * + https://clang.llvm.org/docs/LanguageExtensions.html#builtin-functions
+ */
+
+#        define m3c_memcpy(dest, src, count) __builtin_memcpy(dest, src, count)
+#        define m3c_memmove(dest, src, count) __builtin_memmove(dest, src, count)
+
+#    endif /* M3C_GNUC || M3C_CLANG */
+
+#endif /* M3C_FEATURE_USE_COMPILER_BUILTIN_FUNCTIONS */
 
 /**
  * \brief Fills `dest` with the contents of `src`. The specified `len` and `count` parameters
