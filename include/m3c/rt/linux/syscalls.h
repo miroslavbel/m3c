aa25b73179e81ee2f46be2cebc4b4aeb6f997d09
Belov Miroslav <miroslavbelov8@gmail.com>
2024-01-31 22:16:59 +0400

    [M3C-92][rt] add syscall wrappers for linux

--- include/m3c/rt/linux/syscalls.h
+++ include/m3c/rt/linux/syscalls.h
@@ -2,6 +2,21 @@
 #define _M3C_INCGUARD_RT_LINUX_SYSCALLS_H
 
 #include <m3c/common/babel.h>
+#include <m3c/common/macros.h>
+
+#include <syscall.h> /* for syscall numbers */
+#include <errno.h>   /* for syscall errors */
+
+/* some headers for convenient syscalls use */
+#include <fcntl.h>    /* for open */
+#include <sys/mman.h> /* for mmap */
+
+/**
+ * \brief Checks if result returned from syscall is an error.
+ *
+ * \see https://stackoverflow.com/a/18998521
+ */
+#define M3C_IsRawErrno(x) M3C_InRange((long)(x), -4095, -1)
 
 /**
  * \brief Performs a syscall with 6 arguments.
@@ -89,5 +104,56 @@ m3c_syscall6(long sysno, long arg1, long arg2, long arg3, long arg4, long arg5, 
  * \return syscall result
  */
 #define m3c_syscall0(sysno) m3c_syscall1((sysno), 0)
+
+#ifdef SYS_open
+/**
+ * \brief Raw wrapper for `open` syscall.
+ *
+ * \details See https://man7.org/linux/man-pages/man2/open.2.html
+ *
+ * \param[in] path  pathname
+ * \param     flags flags
+ *
+ * \return
+ * + on error - errno (see #M3C_IsRawErrno)
+ * + on success - fd
+ */
+int m3c_syscall_open(const char *path, int flags);
+#endif /* SYS_open */
+
+#ifdef SYS_close
+/**
+ * \brief Raw wrapper for `close` syscall.
+ *
+ * \details See https://man7.org/linux/man-pages/man2/close.2.html
+ *
+ * \param fd file descriptor
+ *
+ * \return
+ * + on error - errno (see #M3C_IsRawErrno)
+ * + on success - `0`
+ */
+int m3c_syscall_close(int fd);
+#endif /* SYS_close */
+
+#ifdef SYS_mmap
+/**
+ * \brief Raw wrapper for `mmap` syscall.
+ *
+ * \details See https://man7.org/linux/man-pages/man2/mmap.2.html
+ *
+ * \param addr   mapping address
+ * \param length mapping length
+ * \param prot   mapping memory protection flags
+ * \param flags  mapping update flags
+ * \param fd     file descriptor
+ * \param offset mapping offset
+ *
+ * \return
+ * + on error - errno (see #M3C_IsRawErrno)
+ * + on success - address of mapping
+ */
+void *m3c_syscall_mmap(void *addr, long length, int prot, int flags, int fd, long offset);
+#endif /* SYS_mmap */
 
 #endif /* _M3C_INCGUARD_RT_LINUX_SYSCALLS_H */
