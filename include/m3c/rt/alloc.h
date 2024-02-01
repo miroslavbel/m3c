392cebae258a97e9cb3701573e7a83b4a1876804
Belov Miroslav <miroslavbelov8@gmail.com>
2024-02-01 15:56:36 +0400

    [M3C-92][rt] add M3C_FEATURE_API_SYSCALLS to alloc.h

--- include/m3c/rt/alloc.h
+++ include/m3c/rt/alloc.h
@@ -1,12 +1,22 @@
 #ifndef _M3C_INCGUARD_RT_ALLOC_H
 #define _M3C_INCGUARD_RT_ALLOC_H
 
-#include <stdlib.h>
+#ifdef M3C_FEATURE_API_STD
 
-#define m3c_malloc(size) malloc(size)
+#    include <stdlib.h>
 
-#define m3c_realloc(ptr, size) realloc(ptr, size)
+#    define m3c_malloc(size) malloc(size)
+#    define m3c_realloc(ptr, size) realloc(ptr, size)
+#    define m3c_free(ptr) free(ptr)
 
-#define m3c_free(ptr) free(ptr)
+#elif defined(M3C_FEATURE_API_SYSCALLS)
+
+#    include <m3c/rt/runtime.h>
+
+#    define m3c_malloc(size) __M3C_Runtime_Malloc((size))
+#    define m3c_realloc(ptr, size) __M3C_Runtime_Realloc((ptr), (size))
+#    define m3c_free(ptr) __M3C_Runtime_Free((ptr))
+
+#endif /* M3C_FEATURE_API_? */
 
 #endif /* _M3C_INCGUARD_RT_ALLOC_H */
